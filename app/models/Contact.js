d75879bc7826f8d3935aef3bd39d31ce29e04652
Erik Jönsson <efjonsson@gmail.com>
2016-10-23 16:59:22 -0400

    add id to contact

--- app/models/Contact.js
+++ app/models/Contact.js
@@ -3,15 +3,16 @@ const request = require('superagent');
 class Contact {
   static all() {
     const contacts = { contacts: [
-      new Contact({ name: 'Erik', email: 'efjonsson@gmail.com', message: 'I want to hire you.' }),
-      new Contact({ name: 'Lichard', email: 'lichard@gmail.com', message: 'I love Omily.' }),
-      new Contact({ name: 'Joy', email: 'joy@gmail.com', message: 'What\' up?' }),
+      new Contact({ id: 1, name: 'Erik', email: 'efjonsson@gmail.com', message: 'I want to hire you.' }),
+      new Contact({ id: 2, name: 'Lichard', email: 'lichard@gmail.com', message: 'I love Omily.' }),
+      new Contact({ id: 3, name: 'Joy', email: 'joy@gmail.com', message: 'What\' up?' }),
     ] };
     return request.get('http://localhost:8080')
            .then(response => contacts)
            .catch(err => err);
   }
-  constructor({ name, email, message }) {
+  constructor({ id, name, email, message }) {
+    this.id = id;
     this.name = name;
     this.email = email;
     this.message = message;
