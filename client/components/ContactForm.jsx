1b2838c492ddba56be63b3391b95d1677448ac14
Erik Jönsson <efjonsson@gmail.com>
2016-10-23 17:16:38 -0400

    handle input change and store in state

--- client/components/ContactForm.jsx
+++ client/components/ContactForm.jsx
@@ -1,6 +1,25 @@
 import React, { Component } from 'react';
 
 class ContactForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      email: '',
+      message: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(e) {
+    const key = e.target.name;
+    const newState = {};
+    newState[key] = e.target.value;
+    this.setState(newState);
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+  }
   render() {
     return (
       <section className="contact_section container">
@@ -8,13 +27,33 @@ class ContactForm extends Component {
           <h2>Contact Erik</h2>
           <form
             className="contact-form"
-            action="https://formspree.io/efjonsson@gmail.com"
-            method="POST"
+            onSubmit={this.handleSubmit}
           >
-            <label htmlFor="name">Name</label><input type="text" name="name" required />
-            <label htmlFor="email">Email</label><input type="email" name="email" required />
+            <label htmlFor="name">Name</label>
+            <input
+              type="text"
+              name="name"
+              onChange={this.handleChange}
+              value={this.state.name}
+              required
+            />
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              name="email"
+              onChange={this.handleChange}
+              value={this.state.email}
+              required
+            />
             <label htmlFor="message">Message</label>
-            <textarea name="message" rows="4" cols="50" required />
+            <textarea
+              name="message"
+              rows="4"
+              cols="50"
+              onChange={this.handleChange}
+              value={this.state.message}
+              required
+            />
             <input type="hidden" name="_subject" value="New message from your website" />
             <input className="submit" type="submit" name="submit" value="Email me" />
           </form>
