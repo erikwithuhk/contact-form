34d203c0bcbb3824cbf4f07be7bca2a8d538d42b
Erik Jönsson <efjonsson@gmail.com>
2016-10-23 16:58:38 -0400

    return mock contacts from get request

--- app/controllers/ContactController.js
+++ app/controllers/ContactController.js
@@ -2,7 +2,9 @@ const Contact = require('../models/Contact');
 
 class ContactController {
   static index(req, res) {
-    res.status(200).send('Contacts');
+    Contact.all()
+           .then(response => res.status(200).json(response))
+           .catch(err => err);
   }
 }
 
--- app/models/Contact.js
+++ app/models/Contact.js
@@ -1,3 +1,22 @@
+const request = require('superagent');
+
 class Contact {
-  
+  static all() {
+    const contacts = { contacts: [
+      new Contact({ name: 'Erik', email: 'efjonsson@gmail.com', message: 'I want to hire you.' }),
+      new Contact({ name: 'Lichard', email: 'lichard@gmail.com', message: 'I love Omily.' }),
+      new Contact({ name: 'Joy', email: 'joy@gmail.com', message: 'What\' up?' }),
+    ] };
+    return request.get('http://localhost:8080')
+           .then(response => contacts)
+           .catch(err => err);
+  }
+  constructor({ name, email, message }) {
+    this.name = name;
+    this.email = email;
+    this.message = message;
+    this.createdAt = new Date();
+  }
 }
+
+module.exports = Contact;
