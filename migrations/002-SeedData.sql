a3d154c75dba753a16f24aebce1194b793d5a59d
Erik Jönsson <efjonsson@gmail.com>
2016-10-24 12:29:26 -0400

    create seed data

---
+++ migrations/002-SeedData.sql
@@ -0,0 +1,5 @@
+\c contact_form
+
+INSERT INTO messages (name, email, body) VALUES ('Erik Jönsson', 'efjonsson@gmail.com', 'This is my first message');
+INSERT INTO messages (name, email, body) VALUES ('Erik Jönsson', 'efjonsson@gmail.com', 'This is my second message');
+INSERT INTO messages (name, email, body) VALUES ('Erik Jönsson', 'efjonsson@gmail.com', 'This is my third message');
