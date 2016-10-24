2898332f310d74aebad7492547d10894de7b34ac
Erik Jönsson <efjonsson@gmail.com>
2016-10-24 17:35:32 -0400

    set up message list to get messages and render message components for each

--- client/components/Admin.jsx
+++ client/components/Admin.jsx
@@ -1,11 +1,43 @@
 import React, { Component } from 'react';
+import request from 'superagent';
+import Message from './Message.jsx';
 
 const propTypes = {};
 
 class Admin extends Component {
+  constructor() {
+    super();
+    this.state = {
+      messages: [],
+    };
+  }
+  componentDidMount() {
+    this.getMessages();
+  }
+  getMessages() {
+    request.get('/api/v1/messages')
+           .then(response => this.setState({ messages: response.body }))
+           .catch(err => err);
+  }
+  renderMessageListItems() {
+    return this.state.messages.map(message => (
+      <Message
+        key={message.id}
+        id={message.id}
+        sender={message.name}
+        senderEmail={message.email}
+        messageBody={message.body}
+      />
+    ));
+  }
   render() {
+    const messageListItems = this.renderMessageListItems();
     return (
-      <h1>Admin</h1>
+      <div className="admin-container">
+        <ul className="message-list">
+          {messageListItems}
+        </ul>
+      </div>
     );
   }
 }
---
+++ client/components/Message.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+const propTypes = {
+
+};
+
+class Message extends Component {
+  render() {
+    return (
+      <li>
+        Message
+      </li>
+    );
+  }
+}
+
+Message.propTypes = propTypes;
+
+export default Message;
